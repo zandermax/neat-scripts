1f401f30f069dbf229ae2ace203e4f994a21a1ae
Alexander Maxwell <alexander.maxwell@check24.de>
2024-07-30 16:49:54 +0200

    Updated yarn script to only build and not pull

--- yarn.zsh
+++ yarn.zsh
@@ -1,25 +1,15 @@
 # Pull all changes in all git repos in a directory
-pull-and-build-all-npm() {
+build-all-npm() {
 	# Initialize an empty string to store directories with errors
 	error_dirs=""
 	# Loop through all subdirectories
 	for d in *-npm-*/; do
-		if [ -d "$d/.git" ]; then # Check if the directory is a Git repository
-			if [ "$(cd "$d" && git rev-parse --abbrev-ref HEAD)" != "master" ]; then
-				echo "Not on master branch in $d, skipping"
-				continue
-			fi
-			echo "Pulling in $d"
-			# Attempt to pull, redirecting errors to a temp file
-			if ! (cd "$d" && git stash && git pull) 2>/tmp/error$$; then
-				echo "Error in $d"
-				# Append the directory and error message to the error_dirs string
-				error_dirs+="$d: $(cat /tmp/error$$)\n"
-			# if no errors, install and build
-			else
-				echo "Running yarn install and yarn build in $d"
-				(cd "$d" && yarn install && yarn build)
-			fi
+		echo "Running yarn install and yarn build in $d"
+		# Attempt to pull, redirecting errors to a temp file
+		if ! (cd "$d" && yarn install && yarn build) 2>/tmp/error$$; then
+			echo "Error in $d"
+			# Append the directory and error message to the error_dirs string
+			error_dirs+="$d: $(cat /tmp/error$$)\n"
 		fi
 	done
 
@@ -27,7 +17,7 @@ pull-and-build-all-npm() {
 	if [ -n "$error_dirs" ]; then
 		echo -e "Errors occurred in the following directories:\n$error_dirs"
 	else
-		echo "Pull and build successful in all directories."
+		echo "Built all npm projects successfully"
 	fi
 
 	# Clean up the temporary error file
