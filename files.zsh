9d493d1933cc6910c018247e879e440d1925aa01
Zander Maxwell <whatscomingnext@gmail.com>
2024-11-22 13:33:05 +0100

    Added mln

--- files.zsh
+++ files.zsh
@@ -1,10 +1,10 @@
 # Function to show counts of files per directory, with headers in output
-count-files() {
-	create-headers "Count" "Directory"
-	find . -mindepth 2 -type f | cut -d/ -f2 | sort | uniq -c | sort -nr | 2-column-output
+count_files() {
+	create_headers "Count" "Directory"
+	find . -mindepth 2 -type f | cut -d/ -f2 | sort | uniq -c | sort -nr | 2_column_output
 }
 
-count-files-of-type() {
+count_files_of_type() {
 	local extensions="$1"
 	local IFS=','
 
@@ -21,6 +21,53 @@ count-files-of-type() {
 	done
 
 	# Find and count files
-	create-headers "Count" "Extension"
-	find . -type f | grep -E "$pattern" | sed -e 's/.*\.//' | sort | uniq -c | sort -nr | 2-column-output
+	create_headers "Count" "Extension"
+	find . -type f | grep -E "$pattern" | sed -e 's/.*\.//' | sort | uniq -c | sort -nr | 2_column_output
+}
+
+# Function to iterate over directories and execute a command
+execute_in_dirs() {
+	local pattern=$1
+	local command=$2
+	echo "Pattern: $pattern"
+	echo "Command: $command"
+	echo "ls -d $pattern"
+	error_dirs=""
+	# Use find to expand the pattern and iterate over matching directories
+	result=$(ls -d $pattern)
+	echo "Result: $result"
+	ls -d "$pattern" | while read -r d; do
+		echo "Executing in $d"
+		if ! (cd "$d" && eval "$command") 2>/tmp/error$$; then
+			log_error "$d" /tmp/error$$
+		fi
+		echo ""
+	done
+
+	if [ -n "$error_dirs" ]; then
+		echo -e "Errors occurred in the following directories:\n$error_dirs"
+	else
+		echo "Operation completed successfully"
+	fi
+
+	rm -f /tmp/error$$
+}
+
+# Moves a directory to a new location and creates a symlink to the new location
+mln() {
+	if [ $# -ne 2 ]; then
+		echo "Usage: mln <directory> <destination>"
+		return 1
+	fi
+
+	local dir=$1
+	local dest=$2
+
+	if [ ! -d "$dir" ]; then
+		echo "Error: Directory '$dir' does not exist."
+		return 1
+	fi
+
+	mv "$dir" "$dest"
+	ln -s "$dest/$(basename "$dir")" "$dir"
 }
